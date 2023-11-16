da28418c95015b1431fe9dcb10b84de57677d6d7
Revi1337 <david122123@gmail.com>
2023-11-17 01:10:53 +0900

    [DataAccess1 - 프로젝트 생성]

---
+++ SpringDataAccess_1/src/main/java/access/springdataaccess_1/SpringDataAccess1Application.java
@@ -0,0 +1,13 @@
+package access.springdataaccess_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataAccess1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataAccess1Application.class, args);
+    }
+
+}
---
+++ SpringDataAccess_1/src/test/java/access/springdataaccess_1/SpringDataAccess1ApplicationTests.java
@@ -0,0 +1,13 @@
+package access.springdataaccess_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataAccess1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
