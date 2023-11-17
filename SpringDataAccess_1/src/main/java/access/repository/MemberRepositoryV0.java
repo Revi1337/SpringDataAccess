386c102e07a3db012fef089a3bcd07dd7489d866
Revi1337 <david122123@gmail.com>
2023-11-17 14:22:05 +0900

    [DataAccess1 - JDBC 이해 - JDBC 개발 - 조회]

--- SpringDataAccess_1/src/main/java/access/repository/MemberRepositoryV0.java
+++ SpringDataAccess_1/src/main/java/access/repository/MemberRepositoryV0.java
@@ -6,7 +6,9 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.NoSuchElementException;
 
 /**
  * V0. JDBC - DriverManager 를 사용한 DB 저장 (Low Level)
@@ -14,6 +16,12 @@ import java.sql.SQLException;
 @Slf4j
 public class MemberRepositoryV0 {
 
+    /**
+     * 데이터 변경은 executeUpdate()
+     * @param member
+     * @return
+     * @throws SQLException
+     */
     public Member save(Member member) throws SQLException {
         String insertSql = "insert into member(member_id, money) values (?, ?)";
 
@@ -26,6 +34,38 @@ public class MemberRepositoryV0 {
             int resultCount = preparedStatement.executeUpdate();
 
             return member;
+        } catch (SQLException e) {
+            log.error("DB Error", e);
+            throw e;
+        }
+    }
+
+    /**
+     * 데이터 조회는 executeQuery() --> 결과는 ResultSet
+     *
+     * ResultSet 은 테이블과 같은 구조. (row, column 의 집합)
+     * ResultSet 에서 데이터를 얻어올려면 매번 .next() 를 호출하여 row 가 있냐? 라고 물어봐야함. (true 있음. false 없음)
+     * getString(column_name), getInt(column_name) 로 컬럼값을 가져올 수 있다.
+     *
+     * @param memberId
+     * @return
+     */
+    public Member findById(String memberId) throws SQLException {
+        String findSql = "select * from member where member_id = ?";
+        try (
+                Connection connection = getConnection();
+                PreparedStatement preparedStatement = connection.prepareStatement(findSql)
+        ) {
+            preparedStatement.setString(1, memberId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                Member member = new Member();
+                member.setMemberId(resultSet.getString("member_id"));
+                member.setMoney(resultSet.getInt("money"));
+                return member;
+            } else {
+                throw new NoSuchElementException("member not found memberId = " + memberId);
+            }
         } catch (SQLException e) {
             log.error("DB Error", e);
             throw e;
--- SpringDataAccess_1/src/test/java/access/repository/MemberRepositoryV0Test.java
+++ SpringDataAccess_1/src/test/java/access/repository/MemberRepositoryV0Test.java
@@ -2,11 +2,16 @@ package access.repository;
 
 
 import access.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.*;
+
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 memberRepositoryV0 = new MemberRepositoryV0();
@@ -14,8 +19,16 @@ class MemberRepositoryV0Test {
     @Test
     @DisplayName("CRUD 테스트")
     public void crud() throws SQLException {
+        // create
         Member member = new Member("memberV1", 10000);
         memberRepositoryV0.save(member);
+
+        // read
+        Member findMember = memberRepositoryV0.findById(member.getMemberId());
+        log.info("findMember = {}", findMember);
+        log.info("member == findMembe {}", member == findMember);
+        log.info("member.equals(findMembe) {}", member.equals(findMember));
+        assertThat(findMember).isEqualTo(member);
     }
 
 }
