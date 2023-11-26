601cb5bc438be1a1eb367dde0fdda2f7fa077ce5
Revi1337 <david122123@gmail.com>
2023-11-26 21:56:02 +0900

    [DataAccess1 - 스프링과 문제해결 - 트랜잭션 문제해결 - 트랜잭션 템플릿]

---
+++ SpringDataAccess_1/src/main/java/access/service/MemberServiceV3_2.java
@@ -0,0 +1,54 @@
+package access.service;
+
+import access.domain.Member;
+import access.repository.MemberRepositoryV3;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - 트랜잭션 템플릿
+ */
+@Slf4j
+public class MemberServiceV3_2 {
+
+    private final MemberRepositoryV3 memberRepository;
+    private final TransactionTemplate transactionTemplate;
+
+    public MemberServiceV3_2(MemberRepositoryV3 memberRepository, PlatformTransactionManager platformTransactionManager) {
+        this.memberRepository = memberRepository;
+        transactionTemplate = new TransactionTemplate(platformTransactionManager);
+    }
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        transactionTemplate.executeWithoutResult(transactionStatus -> {
+            try {
+                // 비지니스 로직
+                bizLogic(fromId, toId, money);
+            } catch (SQLException e) {
+                throw new IllegalStateException(e);
+            }
+        });
+    }
+
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체 중 예외 발생");
+        }
+    }
+
+}
