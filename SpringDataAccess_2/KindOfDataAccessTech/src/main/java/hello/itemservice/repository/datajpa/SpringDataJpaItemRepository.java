d281089231c7d37dda5c52a985233116e214c13b
Revi1337 <david122123@gmail.com>
2023-12-16 01:57:46 +0900

    [DataAccess2 - 데이터 접근 기술 - DataJPA - 적용1]

---
+++ SpringDataAccess_2/KindOfDataAccessTech/src/main/java/hello/itemservice/repository/datajpa/SpringDataJpaItemRepository.java
@@ -0,0 +1,24 @@
+package hello.itemservice.repository.datajpa;
+
+import hello.itemservice.domain.Item;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+
+public interface SpringDataJpaItemRepository extends JpaRepository<Item, Long> {
+
+    List<Item> findByItemNameLike(String itemName);
+
+    List<Item> findByPriceLessThanEqual(Integer price);
+
+    // QueryMethod
+    List<Item> findByItemNameLikeAndPriceLessThanEqual(String itemName, Integer price);
+
+    // JPQL
+    @Query("select i from Item as i where i.itemName like :itemName and i.price <= :price")
+    List<Item> findItems(@Param("itemName") String itemName, @Param("price") Integer price);
+
+}
